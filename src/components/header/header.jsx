15d5f4adbbf3759ab82621adfe36054c13aeed46
Erkosha <erkebulan003@yandex.ru>
2023-11-18 02:33:28 +0600

    add header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.svg";
+import PersonPhoto from "../../assets/person.svg";
+import CartPhoto from "../../assets/cart.svg";
+import styles from "./header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.nav}>
+        <ul>
+          <li>
+            <Link to="/home" className={styles.home}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/collection" className={styles.collection}>
+              Collection
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={styles.about}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className={styles.contact}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.logo}>
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className={styles.search}>
+        <img src={PersonPhoto} alt="" />
+        <img src={CartPhoto} alt="" />
+        <input type="text" placeholder="Search..." />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
